8498769e742f33d58089da4d2dd689741899bf42
Rahul Rehan <16460020+rahulrehan20@users.noreply.github.com>
2026-06-04 19:25:33 +0530

    Reduce app caching

--- api.php
+++ api.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 
 header('Content-Type: application/json');
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: 0');
 
 $dataDir = __DIR__ . DIRECTORY_SEPARATOR . 'data';
 $dataFile = $dataDir . DIRECTORY_SEPARATOR . 'meal-planner.json';
